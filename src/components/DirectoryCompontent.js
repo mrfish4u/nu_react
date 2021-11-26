fd67d01b1583faf59750eeb034e0a6890d7a8375
Joe Brawley <dev@brawleytech.com>
2021-11-26 10:20:23 -0700

    Week3 Adding Single Page App

--- src/components/DirectoryCompontent.js
+++ src/components/DirectoryCompontent.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-function RenderDirectoryItem({campsite, onClick}) {
+function RenderDirectoryItem({campsite}) {
     return (
-        <Card onClick={() => onClick(campsite.id)}>
+        <Card>
             <CardImg width="100%" src={campsite.image} alt={campsite.name} />
             <CardImgOverlay>
                 <CardTitle>{campsite.name}</CardTitle>
@@ -16,7 +17,7 @@ function Directory (props) {
     const directory = props.campsites.map(campsite => {
         return (
             <div key={campsite.id} className="col-md-5 m-1">
-                <RenderDirectoryItem campsite={campsite} onClick={props.onClick} />
+                <RenderDirectoryItem campsite={campsite} />
             </div>
         );
     });
@@ -24,10 +25,18 @@ function Directory (props) {
     return (
         <div className="container">
             <div className="row">
-                {directory}
+                <div className="col">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to ="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem><Link active>Directory</Link></BreadcrumbItem>
+                    </Breadcrumb>
+                    <h2>Directory</h2>
+                    <hr />
+                </div>
             </div>
-            <div className="row">
-            </div>
+                <div className="row">
+                    {directory}
+                </div>
         </div>
     );
 }
